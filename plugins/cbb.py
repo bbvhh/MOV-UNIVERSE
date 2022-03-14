113c508520bcb4d4452f0944f6562454481faf24
Kiruthikvarma55 <75213972+Kiruthikvarma55@users.noreply.github.com>
2022-03-14 22:42:15 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,12 +8,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🪄 Creator : KiruthiVarma \n🎓 Language : <code>Python3</code>\n✅️ Source Code : <a href='https://github.com/kiruthikvarma55/MOV-UNIVERSE'>Click here</a>\n\n<b>🔥 Powered by, \n@OYETC</b>",
+            text = f"<b>🪄 Creator : KiruthiVarma \n\n🎓 Language : <code>Python3</code>\n\n✅️ Source Code : <a href='https://github.com/kiruthikvarma55/MOV-UNIVERSE'>Click here</a>\n\nHosting: Heroku (Free)\n\n<b>🔥 Powered by, \n@OYETC.</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔙 BACK 🔙", callback_data = "start")
+                        InlineKeyboardButton("CLOSE", callback_data = "close")
                     ]
                 ]
             )
